61e278575c4d72b68c1abe9f312db280f7969d19
Emanuel <123523520+emanueldias01@users.noreply.github.com>
2025-08-12 18:28:16 -0300

    add form

--- src/screens/Forms/Forms.tsx
+++ src/screens/Forms/Forms.tsx
@@ -1,11 +1,51 @@
-import { Text, View } from "react-native";
+import { Alert, Image, Pressable, Text, TextInput, View } from "react-native";
 import styles from "./FormsStyle";
+import returnImg from '../../../assets/return.png';
+import { RootStackParamList } from "../../../routes";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { useNavigation } from "@react-navigation/native";
 
+type NavigationProp = NativeStackNavigationProp<RootStackParamList, 'Forms'>;
 
 export default function Forms(){
+
+    const navigation = useNavigation<NavigationProp>();
+
+    const handlerSend = () => {
+        Alert.alert("Emergência enviada com sucesso!");
+    }
+
+    const handleReturnHome = () => {
+        navigation.goBack();
+    }
+
     return(
         <View style={styles.formsContainer}>
-            <Text>Forms</Text>
+            <Text style={styles.text}>Tipo de emergência:</Text>
+            <TextInput placeholder="Crime" style={styles.inputText}/>
+
+            <Text style={styles.text}>Localização:</Text>
+            <TextInput style={styles.inputText} placeholder="Bairro"/>
+
+            <Text style={styles.text}>Horário do ocorrido:</Text>
+            <TextInput style={styles.inputText} placeholder="Data"/>
+
+            <Text style={styles.text}>Descrição:</Text>
+            <TextInput style={styles.inputTextDescription} 
+                multiline
+                numberOfLines={6}
+            placeholder="criminoso/situação"/>
+
+            <View style={styles.buttonContainer}>
+                <Pressable onPress={handlerSend} style={styles.sendButton}>
+                    <Text style={styles.textButton}>Enviar</Text>
+                </Pressable>
+
+                <Pressable onPress={handleReturnHome} style={styles.returnButton}>
+                    <Image style={styles.imgReturnButton} source={returnImg}/>
+                </Pressable>
+            </View>
+            
         </View>
     );
 }
--- src/screens/Forms/FormsStyle.tsx
+++ src/screens/Forms/FormsStyle.tsx
@@ -4,9 +4,64 @@ import { StyleSheet } from "react-native";
 const styles = StyleSheet.create({
     formsContainer: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
+        marginTop: 100,
+        alignItems: 'flex-start',
+        margin: 35,
         alignContent: 'center'
+    },
+    text: {
+        fontSize: 15,
+        fontWeight: 'bold',
+    },
+
+    inputText: {
+        paddingHorizontal: 30,
+        width: '90%',
+        paddingVertical: 10,
+        borderRadius: 15,
+        borderWidth: 3,
+        margin: 7
+    },
+    inputTextDescription: {
+        paddingHorizontal: 30,
+        width: '90%',
+        height: '30%',
+        paddingVertical: 10,
+        borderRadius: 15,
+        borderWidth: 3,
+        margin: 7
+    },
+    buttonContainer: {
+        marginTop: 50,
+        alignItems: 'center',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        width: '100%'
+    },
+    sendButton: {
+        backgroundColor: 'black',
+        paddingHorizontal: 30,
+        paddingVertical: 15,
+        borderRadius: 16,
+    },
+    textButton: {
+        color: 'white',
+        fontSize: 15,
+        fontWeight: 'bold',
+    },
+    returnButton: {
+        height: 45,
+        width: 45,
+        borderWidth: 2,
+        borderColor: 'black',
+        borderRadius: 22.5,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    imgReturnButton: {
+        resizeMode: 'contain',
+        width: 30,
+        height: 30
     }
 });
 
